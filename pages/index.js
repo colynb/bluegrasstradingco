3152e9e834f1697814942ca37a3f7d551143f03e
Colyn <colyn.brown@gmail.com>
2021-10-09 12:44:50 -0400

    Copy updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+const PreOrderButton = () => {
+  return (
+    <button className="p-3 w-full text-opacity-90 px-12 text-lg text-white bg-[#04403F] rounded-lg font-medium uppercase">
+      Pre-Order Yours Now!
+    </button>
+  )
+}
+
 export default function Home() {
   return (
     <div className="h-screen w-screen">
@@ -17,10 +25,10 @@ export default function Home() {
       </header>
 
       <main>
-        <div className="md:grid sm:grid-cols-2">
-          <div className="bg-yellow-50 p-12 md:text-right flex md:justify-end">
+        <div className="lg:grid sm:grid-cols-2">
+          <div className="bg-yellow-50 p-4 lg:p-12 lg:text-right flex lg:justify-end">
             <div className="max-w-xl text-[#04403F]">
-              <h1 className="font-bold font-display text-6xl mb-4 uppercase">
+              <h1 className="font-bold font-display text-4xl lg:text-6xl mb-4 uppercase">
                 Banjo{' '}
                 <span className="text-[#b43b26] whitespace-nowrap">
                   All-Star
@@ -28,7 +36,7 @@ export default function Home() {
                 <br />
                 Trading Cards
               </h1>
-              <p className="leading-loose opacity-75 mb-4">
+              <p className="leading-loose opacity-75 mb-4 text-base md:text-lg">
                 I am currently working several artists to create the
                 world&apos;s first bluegrass related, high-quality, trading
                 cards. These aren&apos;t photo cards, they will be works of art,
@@ -42,16 +50,17 @@ export default function Home() {
                 obvious choices (e.g. Earl Scruggs, J.D., etc) but more, less
                 obvious ones.
               </p>
-
-              <button className="p-4 px-12 text-xl text-white bg-[#04403F] rounded-lg font-bold">
-                Pre-Order Yours Now!
-              </button>
-              <p className="mt-2">
-                <span className="font-bold text-xl">Shipping Spring 2022!</span>
-              </p>
+              <div className="flex lg:justify-end">
+                <div className="w-80">
+                  <PreOrderButton />
+                  <span className="font-bold text-xl">
+                    Shipping Spring 2022!
+                  </span>
+                </div>
+              </div>
             </div>
           </div>
-          <div className="bg-blue-200 relative h-96 md:h-auto">
+          <div className="bg-blue-200 relative h-96 lg:h-auto">
             <Image
               src="/SitePromo.png"
               alt="Bluegrass Trading Co."
@@ -60,52 +69,118 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className="p-12 text-gray-600 text-xl leading-loose">
-          <div className="max-w-4xl mx-auto space-y-4">
-            <div className="opacity-75 space-y-4">
-              <p>
-                Hi, I’m Colyn Brown. I started Bluegrass Trading Company as a
-                way to offer merchandise to bluegrass enthusiasts like myself.
-                Though I’ve only been playing banjo since 2017, I’ve been
-                involved in bluegrass one way or the other my whole life. My
-                dad, Ken Brown, was an award winning banjo player early on in
-                the mid to late 60s Northeast region of the country, hanging out
-                and picking with young guys like Pete Wernick, Bill Keith. I
-                spent a lot of time growing up listening to bluegrass.
-              </p>
-              <p>
-                Why trading cards you ask? Early in 2021 the thought just hit me
-                that these should exist so I asked around to see if I could find
-                them. The overwhelming response online was that I should make
-                these. I wasn&apos;t so sure I could do it. I just knew that I
-                wanted to have the cards exist so that I could have some. They
-                didn’t exist, at least not in the way I imagined, so the only
-                solution was for me to take a stab at it.
-              </p>
-
-              <p>
-                Fast forward a few months of thinking about how to do it and how
-                I would market them, I found Georgina Flood who was thrilled to
-                partner with me on this first few cards. Working with her
-                inspired me to reach out to other artists and I am happy to be
-                working with four different artists each with their own unique
-                style.
-              </p>
-
-              <p>
-                Thank you to everyone who has expressed interest in this
-                project. I am honored to be able to do something like this and
-                produce them for you all. I am accepting pre-orders because
-                these are not cheap to make. Your pre-orders will allow me to
-                offset some of the cost of the paintings as well as the cost of
-                print production. It also gives me a better idea of how many
-                packs I should be producing.
-              </p>
+        <div className="text-gray-600 text-md lg:text-xl leading-loose">
+          <div>
+            <div className="p-4 lg:p-12">
+              <div className="max-w-4xl mx-auto space-y-4 ">
+                <h2 className="font-bold font-display uppercase text-3xl text-[#04403F]">
+                  PROJECT STATUS
+                </h2>
+                <p>In Progress: 2 of 24 portraits have been completed.</p>
+              </div>
             </div>
-            <div className="max-w-md mx-auto">
-              <button className="p-4 w-full text-xl text-white bg-[#04403F] rounded-lg font-bold">
-                Pre-Order Yours Now!
-              </button>
+
+            <div className="p-4 lg:p-12 bg-gray-100">
+              <div className="max-w-4xl mx-auto space-y-4 ">
+                <h2 className="font-bold font-display uppercase text-3xl text-[#04403F]">
+                  ABOUT
+                </h2>
+                <p>
+                  Hi, I’m Colyn Brown. I started Bluegrass Trading Company as a
+                  way to offer merchandise to bluegrass enthusiasts like myself.
+                  Though I’ve only been playing banjo since 2017, I’ve been
+                  involved in bluegrass one way or the other my whole life. My
+                  dad, Ken Brown, was an award winning banjo player early on in
+                  the mid to late 60s Northeast region of the country, hanging
+                  out and picking with young guys like Pete Wernick, Bill Keith.
+                  I spent a lot of time growing up listening to bluegrass.
+                </p>
+                <p>
+                  Why trading cards you ask? Early in 2021 the thought just hit
+                  me that these should exist so I asked around to see if I could
+                  find them. The overwhelming response online was that I should
+                  make these. I wasn&apos;t so sure I could do it. I just knew
+                  that I wanted to have the cards exist so that I could have
+                  some. They didn’t exist, at least not in the way I imagined,
+                  so the only solution was for me to take a stab at it.
+                </p>
+
+                <p>
+                  Fast forward a few months of thinking about how to do it and
+                  how I would market them, I found Georgina Flood who was
+                  thrilled to partner with me on this first few cards. Working
+                  with her inspired me to reach out to other artists and I am
+                  happy to be working with four different artists each with
+                  their own unique style.
+                </p>
+
+                <p>
+                  Thank you to everyone who has expressed interest in this
+                  project. I am honored to be able to do something like this and
+                  produce them for you all. I am accepting pre-orders because
+                  these are not cheap to make. Your pre-orders will allow me to
+                  offset some of the cost of the paintings as well as the cost
+                  of print production. It also gives me a better idea of how
+                  many packs I should be producing. I am grateful for any
+                  support you can provide. Thank you!
+                </p>
+                <div className="max-w-sm">
+                  <PreOrderButton />
+                </div>
+              </div>
+            </div>
+
+            <div className="p-4 lg:p-12">
+              <div className="max-w-4xl mx-auto space-y-4 ">
+                <h2 className="font-bold font-display uppercase text-3xl text-[#04403F]">
+                  ARTISTS
+                </h2>
+                <p>
+                  These are the artists I am working with that have works in
+                  progress. More coming soon.
+                </p>
+                <p>
+                  <strong>Georgina Flood</strong>
+                </p>
+                <p>
+                  Consectetur incididunt mollit reprehenderit consequat sunt
+                  fugiat dolore proident est quis elit laborum. Et Lorem magna
+                  nisi in ullamco est. Commodo veniam irure qui commodo.
+                  Reprehenderit dolor eiusmod ad duis aliqua magna ad ad sit
+                  laborum. Nisi tempor est officia ut consectetur dolore
+                  deserunt exercitation excepteur mollit in.
+                </p>
+
+                <p>
+                  <strong>Kara Lockmiller</strong>
+                </p>
+
+                <p>
+                  Ut quis irure do duis id fugiat sunt voluptate elit nulla
+                  pariatur cillum. Nostrud eiusmod dolore id sit sit proident
+                  duis elit proident irure quis. Mollit ex aute fugiat consequat
+                  reprehenderit occaecat. Elit pariatur pariatur esse
+                  reprehenderit voluptate proident. Sunt labore ipsum est
+                  proident ullamco id. Nulla laborum aliqua veniam elit aute
+                  reprehenderit nulla. Ullamco ex nulla sint commodo cillum
+                  commodo ipsum dolor duis et.
+                </p>
+
+                <p>
+                  <strong>Robert Felker</strong>
+                </p>
+
+                <p>
+                  Laboris ea excepteur enim cillum do consequat est velit duis
+                  qui ea laborum nostrud ea. Sit consequat anim voluptate ea
+                  cupidatat Lorem eu elit ea eu cupidatat qui. Eiusmod culpa
+                  reprehenderit quis sint non aliqua sunt irure irure ipsum id
+                  nisi nostrud minim.
+                </p>
+                <div className="max-w-sm">
+                  <PreOrderButton />
+                </div>
+              </div>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Montserrat', 'sans-serif'],
+        sans: ['Roboto', 'sans-serif'],
         display: ['Roboto Condensed', 'cursive'],
       },
     },
