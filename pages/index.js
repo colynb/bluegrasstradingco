5f5897080f9b117fcfa1e42a4418ed56a0f659e0
Colyn <colyn.brown@gmail.com>
2021-10-09 11:40:44 -0400

    copy update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import EmailSignup from '../components/email-signup'
 
 export default function Home() {
   return (
@@ -62,35 +61,52 @@ export default function Home() {
           </div>
         </div>
         <div className="p-12 text-gray-600 text-xl leading-loose">
-          <div className="max-w-4xl mx-auto opacity-75 space-y-4">
-            <p>
-              Hi, I’m Colyn Brown. I started Bluegrass Trading Company as a way
-              to offer merchandise to the bluegrass enthusiast who is as
-              enthusiastic about bluegrass as I am. Though I’ve only been
-              playing banjo for 4 years, I’ve been involved in one way or the
-              other my whole life. My dad, Ken Brown, was an award winning banjo
-              player early on in the mid to late 60s Northeast region of the
-              country, hanging out and picking with young guys like Pete
-              Wernick, Bill Keith. I spent a lot of time growing up listening to
-              bluegrass.
-            </p>
-            <p>
-              Wait, what, why trading cards you ask? Early in 2021 the thought
-              just hit me that these should exist so I asked around to see if I
-              could find them. The overwhelming response online was that I
-              should make these. I wasn&apos;t so sure I could do it. I just
-              knew that I wanted to have the cards exist so that I could have
-              some. They didn’t exist, at least not in the way I imagined, so
-              the only solution was for me to take a stab at it.
-            </p>
+          <div className="max-w-4xl mx-auto space-y-4">
+            <div className="opacity-75">
+              <p>
+                Hi, I’m Colyn Brown. I started Bluegrass Trading Company as a
+                way to offer merchandise to bluegrass enthusiasts like myself.
+                Though I’ve only been playing banjo since 2017, I’ve been
+                involved in bluegrass one way or the other my whole life. My
+                dad, Ken Brown, was an award winning banjo player early on in
+                the mid to late 60s Northeast region of the country, hanging out
+                and picking with young guys like Pete Wernick, Bill Keith. I
+                spent a lot of time growing up listening to bluegrass.
+              </p>
+              <p>
+                Why trading cards you ask? Early in 2021 the thought just hit me
+                that these should exist so I asked around to see if I could find
+                them. The overwhelming response online was that I should make
+                these. I wasn&apos;t so sure I could do it. I just knew that I
+                wanted to have the cards exist so that I could have some. They
+                didn’t exist, at least not in the way I imagined, so the only
+                solution was for me to take a stab at it.
+              </p>
 
-            <p>
-              Fast forward a few months of thinking about how to do it and how I
-              would market them, I found Georgina Flood who was thrilled to
-              partner with me on this first few cards. Working with her inspired
-              me to reach out to other artists and I am happy to be working with
-              four different artists each with their own unique style.
-            </p>
+              <p>
+                Fast forward a few months of thinking about how to do it and how
+                I would market them, I found Georgina Flood who was thrilled to
+                partner with me on this first few cards. Working with her
+                inspired me to reach out to other artists and I am happy to be
+                working with four different artists each with their own unique
+                style.
+              </p>
+
+              <p>
+                Thank you to everyone who has expressed interest in this
+                project. I am honored to be able to do something like this and
+                produce them for you all. I am accepting pre-orders because
+                these are not cheap to make. Your pre-orders will allow me to
+                offset some of the cost of the paintings as well as the cost of
+                print production. It also gives me a better idea of how many
+                packs I should be producing.
+              </p>
+            </div>
+            <div className="max-w-md mx-auto">
+              <button className="p-4 w-full text-xl text-white bg-[#04403F] rounded-lg font-bold">
+                Pre-Order Yours Now!
+              </button>
+            </div>
           </div>
         </div>
       </main>
