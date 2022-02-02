a6d236bb7dcfb09a75ebbbe2996e4746f5ecb8bc
Colyn <colyn.brown@gmail.com>
2022-02-02 17:53:02 -0500

    List of completed portraits

---
+++ pages/all-stars/series1/players/index.js
@@ -0,0 +1,57 @@
+export default function Player(props) {
+  return (
+    <div className="h-screen w-full flex items-center justify-center">
+      <div>
+        <div>PLAYERS</div>
+      </div>
+    </div>
+  )
+}
+
+/**
+ * Alan Munde
+Alison Brown
+Allen Shelton
+Ashley Campbell
+Ben Clark
+Bill Emerson
+Bill Keith
+Butch Robins
+Carl Jackson
+Catherine “BB” Bowness
+Charlie Poole
+Cory Walker
+Don Reno
+Don Stover
+Don Wayne Reno
+Doug Dillard
+Earl Scruggs
+Eddie Adcock
+Gina Furtado 
+Grandpa Jones 
+Greg Cahill
+J.D. Crowe
+Jason Davis
+Jens Koch
+Jeremy Stephens
+Jim Pankey
+John Apfelthaler
+John Dowling
+John Hartford 
+Ken Brown
+Kristen Scott Benson
+Mike Scott
+Mike Sumner
+Ned Luberecki
+Nick Einterz
+Nick Hornbuckle
+Ralph Stanley
+Robby Boone
+Ron Block
+Scott Vestal
+Snuffy Jenkins
+Sonny Osborne
+Terry Baucom
+Tony Trischka
+Wes Corbett
+ */
